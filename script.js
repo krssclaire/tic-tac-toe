a2b67abd73013e40362d8c6995c0bf7e3a2b1b6d
Kristel <kristelclaire.02@gmail.com>
2023-06-27 13:52:33 +0100

    Add initial window
    Add window at the start of the game so that the players can add their own
    names

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
+const Player = (name, symbol) => {
+    const getName = () => name;
+    const getSymbol = () => symbol;
+    return { getName, getSymbol };
+}
+
 const Gameboard = (() => {
     let gameBoard = ['', '', '', '', '', '', '', '', ''];
     return gameBoard;
 })();
-
-const Player = (symbol) => {
-    const getSymbol = () => symbol;
-    return { getName, getSymbol };
-}
 
 /*
 const playerOne = Player('X');
@@ -14,3 +15,18 @@ const playerTwo = Player('O');
 */
 
 //const displayController = (() => { })();
+
+/*
+> Organization Scheme
+
+player
+gameboard
+renderContent -> from array to webpage
+displayController -> on the right place of the grid on the DOM
+                    Note: no places already taken
+checkWin -> win / game over / tie
+input Players
+start/restart 
+
+
+*/
